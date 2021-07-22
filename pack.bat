21e4fe6195336829ac9cb8fc5f1093520424de1f
Bergi-bear <tarantyl@inbox.ru>
2021-07-22 21:46:07 +0300

    Created

---
+++ pack.bat
@@ -0,0 +1,2 @@
+start mpqtool new ./map.w3x WasdController.w3x
+xcopy "C:\Users\Bergi\IdeaProjects\WasdController\WasdController.w3x" "C:\Users\Bergi\Documents\Warcraft III\Maps\WasdController.w3x" /Y
